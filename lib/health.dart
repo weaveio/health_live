dd10baef6ba8e198af7651479b2521396e5eb044
Rob Gagne <rob@fluxweave.com>
2024-10-30 10:23:42 +0900

    check in

---
+++ lib/health.dart
@@ -0,0 +1,16 @@
+library health;
+
+import 'dart:async';
+import 'dart:collection';
+import 'dart:io' show Platform;
+import 'package:collection/collection.dart';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+part 'src/data_types.dart';
+part 'src/functions.dart';
+part 'src/health_data_point.dart';
+part 'src/health_value_types.dart';
+part 'src/health_factory.dart';
